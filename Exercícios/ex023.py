2c79548a8c85afdf13568df7b4fb080d87220d2a
math-freire <matheus.freire34@hotmail.com>
2022-08-25 21:17:15 -0300

    ex026

--- Exercícios/ex023.py
+++ Exercícios/ex023.py
@@ -1,7 +1,9 @@
-number = input('Enter a number between 1 and 9999: ')
+number = 'abcde'
+while len(number) > 4:  # If greater than 9999
+    number = input('Enter a number between 1 and 9999: ')
 
 print(""" 
 Unit: {}
 Ten: {}
 Hundred: {}
-Thousand: {}""".format(number[0], number[1], number[2], number[3]))
+Thousand: {}""".format(number[3], number[2], number[1], number[0]))
---
+++ Exercícios/ex026.py
@@ -0,0 +1,5 @@
+x = input('Type anything: ')
+
+print('A appears {} times.'.format(x.count('A')))
+print('A appears for the first time at position #{}.'.format(x.find('A')))
+print('A appears for the last time at position #{}.'.format(x.rfind('A')))
