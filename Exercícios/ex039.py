b8ee87e34a00afeb14500ec3b27d06af6c105398
math-freire <matheus.freire34@hotmail.com>
2022-08-31 20:27:49 -0300

    ex039

---
+++ Exercícios/ex039.py
@@ -0,0 +1,14 @@
+import datetime
+
+y = int(input('Age: '))
+
+date = datetime.date.today()  # Dia atual
+year = int(date.strftime("%Y"))  # Ano atual
+
+age = year - y
+if age < 18:
+    print('Safe')
+elif age == 18:
+    print('USA needs you, Bro')
+else:
+    print("Bad news... you should've served")
