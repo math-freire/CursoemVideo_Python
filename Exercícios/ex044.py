c09857e5ff08ddda56d16c404725b0974b65a238
math-freire <matheus.freire34@hotmail.com>
2022-08-31 21:35:24 -0300

    ex044

--- Exercícios/ex044.py
+++ Exercícios/ex044.py
@@ -1,5 +1,5 @@
 price = float(input('Price: '))
-pay = input('1- Dinheiro\n2- Cartão a vista\n3- Cartão em 2x\n4- Cartão em 3x ou mais\n')
+pay = int(input('1- Dinheiro\n2- Cartão a vista\n3- Cartão em 2x\n4- Cartão em 3x ou mais\n'))
 
 if pay == 1:
     print('Price: {:.2f}'.format(price * 0.9))
@@ -7,6 +7,6 @@ elif pay == 2:
     print('Price: {:.2f}'.format(price * 0.95))
 elif pay == 3:
     print('Price: {:.2f}'.format(price))
-else:
-    print('Price: {:.2f}'.format(price * 0.8))
+elif pay == 4:
+    print('Price: {:.2f}'.format(price * 1.2))
 
