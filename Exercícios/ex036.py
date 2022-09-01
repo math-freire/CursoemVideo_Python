4ade846542596a21c238e432533addd20ee03346
math-freire <matheus.freire34@hotmail.com>
2022-08-31 22:13:25 -0300

    Correção ex036

--- Exercícios/ex036.py
+++ Exercícios/ex036.py
@@ -2,9 +2,11 @@ vc = float(input('Enter the house price: '))
 wage = float(input('Enter your wage: '))
 time = float(input('How many years to pay: '))
 
-cost = vc / 12 * time
+cost = vc/(time*12)
 
-if cost > wage * 0.3:
-    print('Your home loan was denied!')
-else:
+if cost <= wage * 0.3:
     print('Your home loan was accepted!')
+    print('Installments: {:.2f}'.format(cost))
+else:
+    print('Your home loan was denied!')
+
