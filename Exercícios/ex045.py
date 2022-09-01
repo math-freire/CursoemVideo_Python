1cb66a98c19ec238d89dfd09c813e1a18fa8a978
math-freire <matheus.freire34@hotmail.com>
2022-08-31 22:02:49 -0300

    ex045 - Jo ken Po

--- Exercícios/ex045.py
+++ Exercícios/ex045.py
@@ -1,9 +1,24 @@
 import random
+import time
 
-choice = int(input('1- Pedra\n2- Papel\n3- Tesoura\n'))
+choice = int(input('\033[33m1- Pedra\033[m\n\033[34m2- Papel\033[m\n\033[35m3- Tesoura\033[m\n'))
 
 pc_choice = random.randrange(1, 4)
-print(pc_choice)
+
+print('JO')
+time.sleep(0.5)
+print('KEN')
+time.sleep(0.5)
+print('PO\n')
+time.sleep(0.5)
+
+if pc_choice == 1:
+    print('PC escolheu Pedra!\n')
+elif pc_choice == 2:
+    print('PC escolheu Papel!\n')
+elif pc_choice == 3:
+    print('PC escolheu Tesoura!\n')
+
 if choice == 1 and pc_choice == 3 or choice == 2 and pc_choice == 1 or choice == 3 and pc_choice == 2:
     print('You won')
 elif choice == pc_choice:
