8125dc354b50a09af346270b39bd04f537064bc7
math-freire <matheus.freire34@hotmail.com>
2022-09-06 21:51:37 -0300

    ex061

---
+++ Exercícios/ex061.py
@@ -0,0 +1,12 @@
+r = int(input('Enter the arithmetic progression rate: '))
+t1 = int(input('Enter the first term: '))
+count, soma = 1, 0
+while count <= 10:
+    if count == 10:
+        print('{}'.format(t1))
+    else:
+        print('{} → '.format(t1), end='')
+    soma += t1
+    t1 += r
+    count += 1
+print('Soma:', soma)
