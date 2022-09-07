5bf21cf555ed5029eec1ded60794ae3b3bd6c250
math-freire <matheus.freire34@hotmail.com>
2022-09-06 23:30:52 -0300

    ex062 - Termos de uma PA

--- Exercícios/ex062.py
+++ Exercícios/ex062.py
@@ -1,17 +1,15 @@
 r = int(input('Enter the arithmetic progression rate: '))
 t1 = int(input('Enter the first term: '))
 soma = 1
-terms = None
+terms, termsCount = None, 0
 
 while terms != 0:
-    terms = int(input('How many terms do you want?'))
+    terms = int(input('How many terms do you want? '))
+    termsCount += terms
     count = 0
     while count < terms:
-        if count == terms-1:
-            print('{}'.format(t1))
-        else:
-            print('{} → '.format(t1), end='')
+        print('{} → '.format(t1) if count != terms-1 else '{} → Pause\n'.format(t1), end='')
         soma += t1
         t1 += r
         count += 1
-print('Soma:', soma)
+print('\n\033[32mSum:\033[0m {}\n\033[32mNumber of terms:\033[0m {}'.format(soma, termsCount))
