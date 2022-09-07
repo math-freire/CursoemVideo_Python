20aaa07ce12c73b06b536368c9a2449599879112
math-freire <matheus.freire34@hotmail.com>
2022-09-06 23:24:38 -0300

    ex061

--- Exercícios/ex061.py
+++ Exercícios/ex061.py
@@ -3,10 +3,7 @@ t1 = int(input('Enter the first term: '))
 count, soma = 1, 0
 
 while count <= 10:
-    if count == 10:
-        print('{}'.format(t1))
-    else:
-        print('{} → '.format(t1), end='')
+    print('{} → '.format(t1) if count != 10 else '{}\n'.format(t1), end='')
     soma += t1
     t1 += r
     count += 1
