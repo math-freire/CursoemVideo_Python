ec31c0d79a5c2416761e7e503485b37b9b9d6e14
math-freire <matheus.freire34@hotmail.com>
2022-09-06 23:50:47 -0300

    ex065

--- Exercícios/ex065.py
+++ Exercícios/ex065.py
@@ -6,8 +6,7 @@ while key != 'n':
     numbers.append(float(input('Enter a number: ')))
     key = input('\033[34mDo you want to continue entering numbers [y/n]?\033[0m\n').strip().lower()
     while key != 'y' and key != 'n':
-        if key != 'n' and key != 'y':
-            print('\033[31mInvalid input\033[0m')
+        print('\033[31mInvalid input\033[0m')
         key = input('\033[34mDo you want to continue entering numbers [y/n]?\033[0m\n').strip().lower()
 
 sleep(0.5)
