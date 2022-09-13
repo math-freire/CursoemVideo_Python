a8c345f0c8645148907b9213d04b6adb4416a6b8
math-freire <matheus.freire34@hotmail.com>
2022-09-13 17:43:19 -0300

    ex079 - Valores únicos em uma Lista

--- Exercícios/ex079.py
+++ Exercícios/ex079.py
@@ -1,7 +1,7 @@
 count = int(input('\033[32mHow many numbers do you want to enter?\n\033[m'))
 nums = []
 for i in range(1, count+1):
-    n = int(input(f'Please, enter number #{i}: '))
+    n = int(input(f'Please, enter number \033[31m#{i}\033[m: '))
     if n not in nums:
         nums.append(n)
 print(f'In ascending order, the numbers are {sorted(nums)}')
