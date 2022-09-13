61c504ebb3ed6a0ed72c651a84e9c3bb1583a9e4
math-freire <matheus.freire34@hotmail.com>
2022-09-12 23:10:11 -0300

    ex077 - Toda palavra é uma lista

--- Exercícios/ex077.py
+++ Exercícios/ex077.py
@@ -2,7 +2,7 @@ words = ('Little', 'Big', 'Bob', 'Melantha', 'La Pluma')
 
 for word in words:
     print(f'\033[31m{word}:\033[m ', end='')
-    for i in range(0, len(word)):
-        if word[i].lower() in 'aeiou':
-            print(word[i], end=' ')
+    for letter in word: # Every word is a list itself
+        if letter.lower() in 'aeiou':
+            print(letter, end=' ')
     print('')
