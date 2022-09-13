24e96772296b438f283c7aaa5b9b105e60114463
math-freire <matheus.freire34@hotmail.com>
2022-09-13 20:20:07 -0300

    Algumas mudanças pequenas

--- Exercícios/ex079.py
+++ Exercícios/ex079.py
@@ -2,6 +2,6 @@ count = int(input('\033[32mHow many numbers do you want to enter?\n\033[m'))
 nums = []
 for i in range(1, count+1):
     n = int(input(f'Please, enter number \033[31m#{i}\033[m: '))
-    if n not in nums:
-        nums.append(n)
+    if n not in nums: nums.append(n)
+    else: print(f'\033[31m{n} is already in the list\033[m')
 print(f'In ascending order, the numbers are {sorted(nums)}')
