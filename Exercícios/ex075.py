eb1144e1c885437ad1d74f395c463e4dea5b2651
math-freire <matheus.freire34@hotmail.com>
2022-09-12 22:54:07 -0300

    ex075 - Melhorado

--- Exercícios/ex075.py
+++ Exercícios/ex075.py
@@ -2,19 +2,10 @@ a, b, c, d = map(int, input('Enter four numbers separated by space:\n').split())
 nums = (a, b, c, d)
 
 print(f'The number 9 appears {nums.count(9)} times.')
-
-if 3 in nums:
-    print(f'The number 3 appears at the #{nums.index(3)+1} position.')
-else:
-    print("\033[31mThe number 3 was not entered.\033[m")
-
+print(f'The number 3 appears at the #{nums.index(3)+1} position.' if 3 in nums else "\033[31mThe number 3 was not "
+                                                                                    "entered.\033[m")
 print('The even numbers entered was: ', end='')
+for n in nums:
+    if n % 2 == 0:
+        print(n, end=' ')
 
-even_nums = ()
-for i in nums:
-    if i % 2 == 0:
-        even_nums += i,
-if even_nums != ():
-    print(f'The even numbers were {even_nums}.')
-else:
-    print('You did not give me any even numbers.')
