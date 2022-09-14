291d9f6146e6a186806fb8484d3ee51f95749349
math-freire <matheus.freire34@hotmail.com>
2022-09-13 22:05:13 -0300

    ex085 - Listas com pares e impares

---
+++ Exercícios/ex085.py
@@ -0,0 +1,8 @@
+par, impar, nums = [], [], []
+for i in range(0, 6):
+    n = int(input(f'Digite o #{i+1} número: '))
+    if n % 2 == 0: par.append(n)
+    else: impar.append(n)
+nums.append(sorted(par))
+nums.append(sorted(impar))
+print(f'Os valores pares são {nums[0]} e os ímpares são {nums[1]}.')
