27841a7d7e57c25cd130d9ad5b4507d9f40b86db
math-freire <matheus.freire34@hotmail.com>
2022-09-15 19:07:42 -0300

    Correções

--- Exercícios/ex090.py
+++ Exercícios/ex090.py
@@ -5,6 +5,12 @@ if results['average'] <= 4:
 else:
     results['condition'] = condition = 'Approved'
 
-print(f"Average: {results['average']}")
 print(f"Name: {results['name']}")
+print(f"Average: {results['average']}")
 print(f"Condition: {results['condition']}")
+
+# or
+
+print('-'*10)
+for k, v in results.items():
+    print(f'- {k} is {v}')
---
+++ Exercícios/ex091b.py
@@ -0,0 +1,18 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogadas = {}
+for i in range(1, 5):
+    jogadas[f'Jogador {i}'] = randint(1, 6)
+
+ranking = []
+for k, v in jogadas.items():
+    print(f"{k} rodou {v}")
+    sleep(0.3)
+print('-='*30)
+sleep(1)
+ranking = sorted(jogadas.items(), key=itemgetter(1))
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}.')
+    sleep(0.3)
+    
