dca551c5207dbece93229af68b7780ad04a6ea17
math-freire <matheus.freire34@hotmail.com>
2022-09-14 23:51:19 -0300

    ex095 (TOP) - Cadastro de jogadores melhorado

--- Exercícios/ex095.py
+++ Exercícios/ex095.py
@@ -10,7 +10,7 @@ def print_table(lista):
         print(f"{count:<5}{x['name']:<15}{str(x['score']):<30}{x['total']:>10}")
     print('\033[1;31m-' * 70, '\033[m')
 
-
+print('\033[1;33mREGISTRATION OF PLAYERS AND GOALS SCORED\033[m')
 players = []
 while True:
     single_player = {'name': input('Name: ')}
@@ -25,18 +25,18 @@ while True:
     while flag not in 'YyNn':
         flag = input('\033[31mInvalid input! Continue [Y/N]? \033[m')
     if flag in 'Nn':
-        sleep(1)
-        print('\033[31m- Shutting down -\033[m')
+        sleep(0.5)
+        print('\033[31m- ...Loading... -\033[m')
         sleep(1)
         break
 
 print_table(players)
 
 while True:
-    n = int(input('\033[31m[999 to end]\033[m Wanna see about which one?'))
+    n = int(input('\033[31m[999 to end]\033[m Wanna see about which one? '))
     if n == 999:
         break
-    if n > len(players) or n < 0:
+    if n > len(players)-1 or n < 0:
         print('\033[31mInvalid input\033[m')
     else:
         print('\033[1;32m-' * 70)
