f1ca5140316b495258d3241d431407468b5c583e
math-freire <matheus.freire34@hotmail.com>
2022-09-16 23:14:14 -0300

    ex104 - Validação de int

---
+++ Exercícios/ex104.py
@@ -0,0 +1,16 @@
+from my_codes import titulo, end_program, endc, red
+
+
+def leiaint():
+    while True:
+        try:
+            n = int(input('Digite um número: '))
+            break
+        except ValueError:
+            print(f'{red()}Erro! Digite um número inteiro!{endc()}')
+    return n
+
+
+n = leiaint()
+print(f'O número inteiro digitado foi {n}.')
+
